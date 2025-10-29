c7764ac6db725a4ade70a2f0797ba363d5fce869
Adaora Nwogbo <adanwogbo@gmail.com>
2025-10-29 17:48:41 -0500

    Update

--- projects/leetcode_sql_50_postgresql_solutions/leetcode_sql_50_postgresql_solutions.sql
+++ projects/leetcode_sql_50_postgresql_solutions/leetcode_sql_50_postgresql_solutions.sql
@@ -16,3 +16,37 @@ ORDER BY product_id;
 
 Link to View Answer: (https://leetcode.com/problems/recyclable-and-low-fat-products/description/?envType=study-plan-v2&envId=top-sql-50)
  
+/* =====================================================
+-- Problem: 002. Find Customer Referee
+-- Question: Find the names of the customer that are either:
+          (1) referred by any customer with id != 2.
+          (2) not referred by any customer.
+    Return the result table in any order.
+-- Difficulty: Easy
+-----------------------------------------------------
+-- Logic:
+-- 1. Select customer names from the Customer table.
+-- 2. Filter where referee_id is not equal to 2.
+-- 3. Include customers with NULL referee_id (not referred).
+===================================================== */
+
+SELECT name
+FROM Customer
+WHERE referee_id != 2
+OR referee_id IS NULL;
+
+Link to View Answer: (https://leetcode.com/problems/find-customer-referee/?envType=study-plan-v2&envId=top-sql-50)
+
+/* =====================================================
+-- Problem: 003. Find Customer Referee
+-- Question: Find the names of the customer that are either:
+          (1) referred by any customer with id != 2.
+          (2) not referred by any customer.
+    Return the result table in any order.
+-- Difficulty: Easy
+-----------------------------------------------------
+-- Logic:
+-- 1. Select customer names from the Customer table.
+-- 2. Filter where referee_id is not equal to 2.
+-- 3. Include customers with NULL referee_id (not referred).
+===================================================== */
