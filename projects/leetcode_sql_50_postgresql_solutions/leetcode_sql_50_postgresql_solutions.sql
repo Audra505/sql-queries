b0d78731abf8f948a0d256200b1eab23be64a13e
Adaora Nwogbo <adanwogbo@gmail.com>
2025-10-29 17:39:21 -0500

    Create leetcode_sql_50_postgresql_solutions.sql

---
+++ projects/leetcode_sql_50_postgresql_solutions/leetcode_sql_50_postgresql_solutions.sql
@@ -0,0 +1,18 @@
+```sql
+/* =====================================================
+-- Problem: 001. Recyclable and Low Fat Products
+-- Question: Find the IDs of products that are both low fat ('Y') and recyclable ('Y').
+-- Difficulty: Easy
+-----------------------------------------------------
+-- Logic:
+-- 1. Filter products where both low_fats and recyclable = 'Y'
+-- 2. Display product_id in ascending order
+===================================================== */
+
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y'
+  AND recyclable = 'Y'
+ORDER BY product_id;
+```
+Link to View Answer: [click here](https://leetcode.com/problems/recyclable-and-low-fat-products/description/?envType=study-plan-v2&envId=top-sql-50)
