f1a124c997ac0ada725dff54e217e8af6c93067d
Adaora Nwogbo <adanwogbo@gmail.com>
2025-10-29 17:40:19 -0500

    Update leetcode_sql_50_postgresql_solutions.sql

--- projects/leetcode_sql_50_postgresql_solutions/leetcode_sql_50_postgresql_solutions.sql
+++ projects/leetcode_sql_50_postgresql_solutions/leetcode_sql_50_postgresql_solutions.sql
@@ -1,4 +1,3 @@
-```sql
 /* =====================================================
 -- Problem: 001. Recyclable and Low Fat Products
 -- Question: Find the IDs of products that are both low fat ('Y') and recyclable ('Y').
@@ -14,5 +13,6 @@ FROM Products
 WHERE low_fats = 'Y'
   AND recyclable = 'Y'
 ORDER BY product_id;
-```
+
 Link to View Answer: [click here](https://leetcode.com/problems/recyclable-and-low-fat-products/description/?envType=study-plan-v2&envId=top-sql-50)
+ Link: https://leetcode.com/problems/find-customer-referee/
